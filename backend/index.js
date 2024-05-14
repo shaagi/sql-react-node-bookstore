5be797172198aed6e4ae1293be09b49bf2dc24a8
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-14 11:46:15 -0400

    made post request for adding a book and then initialized react on client code

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const db = mysql.createConnection({
     database: "test"
 })
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.json("hello this is the backend");
 })
@@ -21,6 +23,20 @@ app.get("/books", (req, res) => {
         return res.json(data)
     })
 });
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title, 
+        req.body.desc, 
+        req.body.cover, 
+    ];
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Book has been created successfully");
+    })
+})
 
 app.listen(8800, ()=>{
     console.log("Connected to backen1d")
