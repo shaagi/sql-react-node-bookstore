4a9d13a99bbfb14145ec8e782c3118f1495473fb
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-15 14:12:37 -0400

    got delete button to work

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -17,6 +17,15 @@ const Books = () => {
     fetchAllBooks();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost:8800/books/" + id);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1>Lama Book Shop</h1>
@@ -27,7 +36,9 @@ const Books = () => {
             <h2>{book.title}</h2>
             <p>{book.desc}</p>
             <span>{book.price}</span>
-            <button className="delete">Delete</button>
+            <button className="delete" onClick={() => handleDelete(book.id)}>
+              Delete
+            </button>
             <button className="update">Update</button>
           </div>
         ))}
