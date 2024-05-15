578d3a0e93363feb68000267195206d64f7012ac
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-15 15:32:41 -0400

    update link redirects properly

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -39,7 +39,9 @@ const Books = () => {
             <button className="delete" onClick={() => handleDelete(book.id)}>
               Delete
             </button>
-            <button className="update">Update</button>
+            <button className="update">
+              <Link to={`/update/${book.id}`}>Update</Link>
+            </button>
           </div>
         ))}
       </div>
